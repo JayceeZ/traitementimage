debc3c8f88605870c9eb199d9e76d0a80e16ed61
Jean-Christophe Isoard <jeanchristophe.isoard@gmail.com>
2015-03-23 08:59:42 +0100

    Modification de int par double pour conserver la proportionalité en cas de valeurs trop petites

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 using namespace cimg_library;
 using namespace std;
 
-#define GRID_SIZE 6
+#define GRID_SIZE 8.0
 
 CImg<unsigned char> blackWhite(CImg<unsigned char> src);
 vector<vector<int> > matrixBW(CImg<unsigned char> image, int x1, int y1, int x2, int y2);
@@ -17,7 +17,7 @@ int main(int argc, const char* argv[]) {
     CImg<unsigned char> src("imgs/couleur.jpg");
 
 	CImg<unsigned char> blackwhite = blackWhite(src);
-    (blackwhite).display("Black & White");
+    //(blackwhite).display("Black & White");
 
     // Test de la méthode de récupération de caractère par contour
 
@@ -25,7 +25,7 @@ int main(int argc, const char* argv[]) {
 
 
     // Test de la méthode de grille
-    CImg<unsigned char> lettera("imgs/lettera.jpg");
+    //CImg<unsigned char> lettera("imgs/lettera.jpg");
 
     vector<vector<int> > matrice(GRID_SIZE*GRID_SIZE);
     //matrice = matrixBW(lettera, 100, 100, 310, 305);
@@ -103,10 +103,10 @@ CImg<unsigned char> blackWhite(CImg<unsigned char> src)
 void drawGrid(CImg<unsigned char>* image, int x1, int y1, int x2, int y2) {
     unsigned char black[] = { 0,0,0 };
 
-    int pixelsColumns = (x2-x1)/GRID_SIZE;
-    int pixelsLines = (y2-y1)/(GRID_SIZE-1);
+    float pixelsColumns = (x2-x1)/GRID_SIZE;
+    float pixelsLines = (y2-y1)/GRID_SIZE;
 
-    for(int j = 0; j < GRID_SIZE; j++) { // pour chaque ligne de la grille
+    for(int j = 0; j <= GRID_SIZE; j++) { // pour chaque ligne de la grille
         for(int i = 0; i < GRID_SIZE; i++) { // Pour chaque cases d'une ligne de la grille
             (*image).draw_rectangle(x1+pixelsColumns*i,y1+j*pixelsLines,x1+(i+1)*pixelsColumns,y1+(j+1)*pixelsLines,black,1,~0U);
         }
@@ -117,14 +117,16 @@ void drawGrid(CImg<unsigned char>* image, int x1, int y1, int x2, int y2) {
 vector<vector<int> > matrixBW(CImg<unsigned char> image, int x1, int y1, int x2, int y2) {
     vector<vector<int> > matrice(GRID_SIZE*GRID_SIZE);
 
-    int pixelsColumns = (x2-x1)/GRID_SIZE;
-    int pixelsLines = (y2-y1)/(GRID_SIZE-1);
+    float pixelsColumns = (x2-x1)/GRID_SIZE;
+    float pixelsLines = (y2-y1)/GRID_SIZE;
+
+    cout << pixelsColumns << " " << pixelsLines << endl;
 
     int nbblanc = 0;
     int nbnoir = 0;
     int valeur;
 
-    for(int j = 0; j < GRID_SIZE; j++) { // pour chaque ligne de la grille
+    for(int j = 0; j <= GRID_SIZE; j++) { // pour chaque ligne de la grille
         for(int i = 0; i < GRID_SIZE; i++) { // Pour chaque cases d'une ligne de la grille
             nbblanc = 0; nbnoir = 0;
             // on réalise le parcours des pixels dont on compte la quantité de noir ou de blanc
